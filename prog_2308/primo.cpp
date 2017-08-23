bb468da836499b8b61a371390897b1ae2918e830
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-23 09:57:13 -0500

    ..

--- prog_2308/primo.cpp
+++ prog_2308/primo.cpp
@@ -5,20 +5,19 @@ using namespace std;
 int main()
 {
     int x;
-    int cont;
+    int cont=0;
     int numero;
     cout << "Introduce un numero" << endl;
     cin >> numero;
-    cont =0;
-    for(x=0;x<numero;x=x+1){
-        if(numero%x ==0){
-            cont=cont+1;
-            if(cont>2)
-                cout<<"El numero ingresado no es par"<<endl;
-            else
-                cout<<"El numero ingresado si es par"<<endl;
-        }
-    }
+    for(x=1;x<numero;x++){
+        if(numero%x ==0)
+            cont=cont+1;}
+    if(cont>2)
+        cout<<"El numero ingresado no es primo"<<endl;
+    else
+        cout<<"El numero ingresado si es primo"<<endl;
+
+
     return 0;
 }
 
