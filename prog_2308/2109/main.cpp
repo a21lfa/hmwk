861294363273b25fc4681ff93c89b4a727cf38d6
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-09-21 09:46:28 -0500

    strcpy and str lenght

---
+++ prog_2308/2109/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int strlen(char *s){
+    char *p =s;
+    while(*p!='\0')
+       p++;
+    return p-s;
+}
+void strcpy(char *s,char *t){
+    //int i;
+    //i = 0;
+    //while((*(s+i) = *(t+i))!='\0')
+        //i++;
+    while((*s++ = *t++));
+}
+int main()
+{
+    char str[]="ciencias";
+    cout<<strlen(str)<<endl;
+    char s[]="ciencia";
+    char t[6]=" ";
+    strcpy(t,s);
+    cout<<t<<endl;
+    return 0;
+}
