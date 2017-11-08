8b7eb40de4364d2bea302d6674a95d42005432cd
a21lfa <31070534+a21lfa@users.noreply.github.com>
2017-11-08 09:13:02 -0500

    Add files via upload

---
+++ prog_2308/Poligono/src/Cuadrado.cpp
@@ -0,0 +1,16 @@
+#include "Cuadrado.h"
+
+Cuadrado::Cuadrado()
+{
+    largo=0;
+}
+Cuadrado::Cuadrado(int x)
+{
+    largo=x;
+}
+int Cuadrado::area(){
+    return largo*largo;
+}
+void Cuadrado::print(){
+    cout<<"el largo es: "<<largo<<endl;
+}
